2db8fc57b8bdaa7a467f1bbfb213b67dc9da7b54
Patrick Niyogitare <54368821+PatrickNiyogitare28@users.noreply.github.com>
2021-09-25 15:54:42 +0200

    ch(angular-scripts)- add remove karma config files

--- angular-scripts/remove-karma-dependencies.sh
+++ angular-scripts/remove-karma-dependencies.sh
@@ -1,1 +1,5 @@
-npm uninstall karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-reporter
+// Remove karma dependencies
+npm uninstall karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-
+
+// Drop karma config files
+rm karma.conf.js
