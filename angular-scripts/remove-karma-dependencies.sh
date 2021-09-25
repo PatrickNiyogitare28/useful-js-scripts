efaed38c19db3229cb5fd6c4f8ca8f62b5e144e7
Patrick Niyogitare <54368821+PatrickNiyogitare28@users.noreply.github.com>
2021-09-25 16:04:31 +0200

    ch(ng-scripts): add rm karma deps with yarn

--- angular-scripts/remove-karma-dependencies.sh
+++ angular-scripts/remove-karma-dependencies.sh
@@ -1,5 +1,10 @@
 // Remove karma dependencies
-npm uninstall karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-
+// With npm
+npm uninstall karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-reporter
+
+// With yarn
+yarn remove karma karma-chrome-launcher karma-coverage-istanbul-reporter karma-jasmine karma-jasmine-html-reporter
+
 
 // Drop karma config files
 rm karma.conf.js
